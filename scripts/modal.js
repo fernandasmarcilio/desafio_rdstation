80f40b3bea982c33cedd525f5e8fafe27725e8de
fernandasmarcilio <fernandas.marcilio@hotmail.com>
2021-07-23 14:41:33 -0300

    visual: added video modal

---
+++ scripts/modal.js
@@ -0,0 +1,9 @@
+const openModal = (id) => {
+  const modal = document.getElementById(id);
+  modal.style.display = "block";
+}
+
+const closeModal = (id) => {
+  const modal = document.getElementById(id);
+  modal.style.display = "none";
+}
